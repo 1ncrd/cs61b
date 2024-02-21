9506956eb99cce5568b2d01736e5722138fab3c8
incrd <incrd@qq.com>
2024-02-21 16:11:17 +0800

    complete phase 1 of proj3

--- proj3/byow/Core/Engine.java
+++ proj3/byow/Core/Engine.java
@@ -2,13 +2,16 @@ package byow.Core;
 
 import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
+import byow.TileEngine.Tileset;
+
+import java.util.Random;
 
 public class Engine {
     TERenderer ter = new TERenderer();
     /* Feel free to change the width and height. */
     public static final int WIDTH = 80;
     public static final int HEIGHT = 30;
-
+    private static Random RANDOM = null;
     /**
      * Method used for exploring a fresh world. This method should handle all inputs,
      * including inputs from the main menu.
@@ -38,15 +41,134 @@ public class Engine {
      * @return the 2D TETile[][] representing the state of the world
      */
     public TETile[][] interactWithInputString(String input) {
-        // TODO: Fill out this method so that it run the engine using the input
-        // passed in as an argument, and return a 2D tile representation of the
-        // world that would have been drawn if the same inputs had been given
-        // to interactWithKeyboard().
-        //
-        // See proj3.byow.InputDemo for a demo of how you can make a nice clean interface
-        // that works for many different input types.
+        /* TODO: Fill out this method so that it run the engine using the input
+             passed in as an argument, and return a 2D tile representation of the
+             world that would have been drawn if the same inputs had been given
+             to interactWithKeyboard().
+
+             See proj3.byow.InputDemo for a demo of how you can make a nice clean interface
+             that works for many different input types.
+        */
+        char option = input.charAt(0);
+        switch (option) {
+            /* Create new world. */
+            case 'N' -> {
+                long seed = Long.parseLong(input.substring(1, input.length() - 1));
+                TETile[][] finalWorldFrame = createWorld(seed);
+                return finalWorldFrame;
+            }
+        }
 
         TETile[][] finalWorldFrame = null;
         return finalWorldFrame;
+    }
+
+    private TETile[][] createWorld(long seed) {
+        RANDOM = new Random(seed);
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        clearWorld(world);
+
+        int walkNum = 30;
+
+        int[] curPos = new int[] {WIDTH / 2, HEIGHT / 2};
+        for (int i = 0; i < walkNum; i++) {
+            curPos = randomWalk(world, curPos[0], curPos[1]);
+        }
+
+        createRooms(world);
+
+        buildWalls(world);
+        return world;
+    }
+
+    private void createRooms(TETile[][] world) {
+        float roomProb = 0.1F;
+        TETile[][] newWorld = new TETile[WIDTH][HEIGHT];
+        clearWorld(newWorld);
+        for (int i = 0; i < WIDTH; i++) {
+            System.arraycopy(world, 0, newWorld, 0, HEIGHT);
+        }
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                if (world[x][y] == Tileset.FLOOR && RandomUtils.bernoulli(RANDOM, roomProb)) {
+                    createRoom(newWorld, x, y);
+                }
+            }
+        }
+        for (int i = 0; i < WIDTH; i++) {
+            System.arraycopy(newWorld, 0, world, 0, HEIGHT);
+        }
+    }
+
+    private void createRoom(TETile[][] world, int x, int y) {
+        int width = RANDOM.nextInt(5, 9);
+        int height = RANDOM.nextInt(5, 9);
+        for (int curX = x - width / 2; curX < x + (width + 1) / 2; curX++) {
+            for (int curY = y - height / 2; curY < y + (height + 1) / 2; curY++) {
+                if (curX >= 1 && curX <= WIDTH - 2 && curY >= 1 && curY <= HEIGHT - 2) {
+                    world[curX][curY] = Tileset.FLOOR;
+                }
+            }
+        }
+    }
+
+    private void buildWalls(TETile[][] world) {
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                if (world[x][y] == Tileset.FLOOR) {
+                    surroundWall(world, x, y);
+                }
+            }
+        }
+    }
+
+    private void surroundWall(TETile[][] world, int x, int y) {
+        int[][] directions = new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
+        for (int i = 0; i < directions.length; i++) {
+            int nextX = x - directions[i][0];
+            int nextY = y - directions[i][1];
+            if (world[nextX][nextY] == Tileset.NOTHING) {
+                world[nextX][nextY] = Tileset.WALL;
+            }
+        }
+    }
+
+    private void clearWorld(TETile[][] world) {
+        // initialize tiles
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+    }
+
+    private int[] randomWalk(TETile[][] world, int x, int y) {
+        int[][] directions = new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int[] direction = directions[RANDOM.nextInt(4)];
+        int length = RANDOM.nextInt(5, 20);
+        int curX = x;
+        int curY = y;
+        for (int i = 0; i < length; i++) {
+            world[curX][curY] = Tileset.FLOOR;
+            curX += direction[0];
+            curY += direction[1];
+            curX = clamp(curX, 1, WIDTH - 2);
+            curY = clamp(curY, 1, HEIGHT - 2);
+        }
+        return new int[] {curX, curY};
+    }
+
+    int clamp(int value, int min, int max) {
+        return Math.max(min, Math.min(max, value));
+    }
+
+    public static void main(String[] args) {
+        Engine engine = new Engine();
+//        String input = "N53335S";
+        String input = "N" + System.currentTimeMillis() + "S";
+        TETile[][] world = engine.interactWithInputString(input);
+
+        engine.ter.initialize(WIDTH, HEIGHT);
+        engine.ter.renderFrame(world);
     }
 }
--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -6,10 +6,109 @@ import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
 import byow.TileEngine.Tileset;
 
+import java.util.Arrays;
 import java.util.Random;
 
 /**
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final int WIDTH = 60;
+    private static final int HEIGHT = 30;
+    private static final long SEED = 2873123;
+    private static final Random RANDOM = new Random(SEED);
+    public int size = 3;
+    public static void main(String[] args) {
+        // initialize the tile rendering engine with a window of size WIDTH x HEIGHT
+        TERenderer ter = new TERenderer();
+
+        ter.initialize(WIDTH, HEIGHT);
+
+        // initialize tiles
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+
+        /* Drawing A Tesselation of Hexagons. */
+        int size = 3;
+        int hexSize = 3;
+
+        int[][] posOfEachHex = getPosOfEachHex(size, hexSize);
+
+        System.out.println(Arrays.deepToString(posOfEachHex));
+
+        for (int[] pos : posOfEachHex) {
+            TETile tile = randomTile();
+            if (pos != null) addHexagon(world, tile, 3, pos[0], pos[1]);
+        }
+
+        // draws the world to the screen
+        ter.renderFrame(world);
+    }
+
+    /**
+     * Add a hexagon to the world at the lower left point (x, y).
+     */
+    private static void addHexagon(TETile[][] world, TETile filler, int size, int x, int y) {
+        int numXTiles = world.length;
+        int numYTiles = world[0].length;
+
+        /* lower part */
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size + i * 2; j++) {
+                world[size - i - 1 + j + x][i + y] = filler;
+            }
+        }
+
+        /* upper part */
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size * 3 - 2 - i * 2; j++) {
+                world[i + j + x][i + y + size] = filler;
+            }
+        }
+    }
+
+    /**
+     *
+     * @param size size of the big hexagon.
+     * @param hexSize size of the small hexagon.
+     * @return The lower left point of each hexagon.
+     */
+    private static int[][] getPosOfEachHex(int size, int hexSize) {
+        int count = ((size + 2 * size - 1) * size) / 2 * 2 - (2 * size - 1);
+        int[][] result = new int[count][];
+
+        int col = 0;
+        int indexCol = 0;
+        int indexAll = 0;
+        while (col <= size / 2 + 1) {
+            while (indexCol < col + size) {
+                int initialY = (size - 1 - col) * hexSize;
+                result[indexAll] = new int[] {col * (hexSize * 2 - 1), initialY + indexCol * hexSize * 2};
+                indexCol += 1;
+                indexAll += 1;
+            }
+            col += 1;
+            indexCol = 0;
+        }
+
+        int maxX = (size * 2 - 1 - 1) * (size * 2 - 1);
+        System.out.println("maxX = " + maxX);
+
+        for (int i = count - (size + 2 * size - 3) * (size - 1) / 2 - 1; i < count; i++) {
+            result[i] = new int[] {maxX - result[count - i - 1][0], result[count - i - 1][1]};
+        }
+
+        return result;
+    }
+
+    private static TETile randomTile() {
+        TETile[] randomTileSet = new TETile[] {Tileset.FLOWER, Tileset.MOUNTAIN, Tileset.TREE, Tileset.GRASS, Tileset.SAND};
+        int tileNum = RANDOM.nextInt(randomTileSet.length);
+        return randomTileSet[tileNum];
+    }
+
 }
